3ff06202680d71283a23213f71420b4393fd191c
gawgua <taisaodeoduoc@gmail.com>
2022-08-06 19:40:50 +0700

    Feature: hardcoded mail (for now)

--- src/server/packets/GetMailCsReq.ts
+++ src/server/packets/GetMailCsReq.ts
@@ -1,14 +1,27 @@
-import { GetMailScRsp } from "../../data/proto/StarRail";
+import { GetMailScRsp, ClientMail, ItemList, Item } from "../../data/proto/StarRail";
 import Packet from "../kcp/Packet";
 import Session from "../kcp/Session";
 
 export default async function handle(session: Session, packet: Packet) {
+    const mailData: ClientMail = {
+        id: 1,
+        templateId: 1,
+        title: "CrepeSR",
+        content: "be the star you want to rail\n\n\t--MemetrollsXD--",
+        sender: "memetolXD + contributors gang",
+        time: Math.round(Date.now() / 1000),
+        attachment: {itemList: []},
+        isRead: false,
+        expireTime: Math.round(Date.now() / 1000) + 10000,
+        paraList: [] 
+    }; //TODO: Mail.json, maybe move to database and add MarkMailRead and DelMail
+
     session.send(GetMailScRsp, {
         retcode: 0,
-        mailList: [],
+        mailList: [mailData],
         noticeMailList: [],
         start: 0,
-        totalNum: 0,
+        totalNum: 1,
         isEnd: false
     } as GetMailScRsp);
 }
