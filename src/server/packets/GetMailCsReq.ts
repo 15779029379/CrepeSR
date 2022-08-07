960dc1f277c6235b4029f83fbfd27036a216fb16
gawgua <taisaodeoduoc@gmail.com>
2022-08-07 17:15:26 +0700

    add mails to config.json

--- src/server/packets/GetMailCsReq.ts
+++ src/server/packets/GetMailCsReq.ts
@@ -1,24 +1,14 @@
 import { GetMailScRsp, ClientMail, ItemList, Item } from "../../data/proto/StarRail";
+import Config from "../../util/Config";
 import Packet from "../kcp/Packet";
 import Session from "../kcp/Session";
 
 export default async function handle(session: Session, packet: Packet) {
-    const mailData: ClientMail = {
-        id: 1,
-        templateId: 1,
-        title: "CrepeSR",
-        content: "be the star you want to rail\n\n\t--MemetrollsXD--",
-        sender: "memetolXD + contributors gang",
-        time: Math.round(Date.now() / 1000),
-        attachment: {itemList: []},
-        isRead: false,
-        expireTime: Math.round(Date.now() / 1000) + 10000,
-        paraList: [] 
-    }; //TODO: Mail.json, maybe move to database and add MarkMailRead and DelMail
+    const mailData = Config.MAILS;
 
     session.send(GetMailScRsp, {
         retcode: 0,
-        mailList: [mailData],
+        mailList: mailData,
         noticeMailList: [],
         start: 0,
         totalNum: 1,
